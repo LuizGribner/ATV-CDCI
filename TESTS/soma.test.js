05d6e2546c6010a4afd7048ed78a84ae44775f43
Luiz <ukangribner@hotmail.com>
2024-05-09 01:30:28 -0300

    New functions add

--- TESTS/soma.test.js
+++ TESTS/soma.test.js
@@ -1,5 +1,26 @@
-const {sum} = require('../soma')
+const {sum, sub, div, mult} = require('../soma')
 
-test('Soma entre dois números', ()=>{
-    expect(sum(2,3)).toBe(5)
-})
+
+    
+        test('Soma entre dois números', () => {
+            expect(sum(2,3)).toBe(5)
+        }) 
+
+    
+        test('Subtração entre dois números', () => {
+            expect(sub(3,5)).toBe(-2)
+        }) 
+    
+    
+        test('Divisão entre dois números', () => {
+            expect(div(10,2)).toBe(5)
+        }) 
+    
+    
+        test('Multiplicação entre dois números', () => {
+            expect(mult(6,8)).toBe(48)
+        }) 
+    
+
+
+
